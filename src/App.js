1bd6364ae64c8003449fe0d866e59449509887dc
Ahmad <ahmad.abdolsaheb@gmail.com>
2017-03-31 15:09:20 +0300

    all fucntions working, console.logs to be removed

--- src/App.js
+++ src/App.js
@@ -5,139 +5,123 @@
 
 import React, { Component } from 'react';
 import './App.css';
-var hidth = 30;
-const levelPlus6 = 6
+
 var levels = [];
-var items = [
-  ['player', 2,1000,1000],
-  ['health', 3],
-  ['stairs', 7],
-  ['weapon', 4],
-  ['enemy', 5,1000,1000,100],
-  ['enemy', 5,1000,1000,100],
-  ['health', 3],
-  ['weapon', 4],
-  ['health', 3],
-  ['enemy', 5,1000,1000,100],
-  ['boss', 6,0,0,1000]
-];
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      cursor : 6,
+      hidth : 30,
       playing: true,
-      health: 100,
+      health: 50,
       enemHealth: 50,
       bossHealth: 500,
       xp: 0,
       weapon: 1,
       level:1,
-      message:"you are playing",
-      grid:this.mapGen()
+      message:"Use the arrow keys to move around.",
+      center:[],
+      grid:[]
     };
-    this.levels = this.levels.bind(this);
+    //this.items = this.items.bind(this);
   }
-  componentWillMount ()
-  {
-    //this.setState({date:this.full()})
+  componentWillMount () {
+    {
+      this.setState({grid:this.mapGen()})
+    }
   }
   componentDidMount () {
   this.centerScreen();
   document.addEventListener('keydown', this.controllerPress.bind(this));
-  //document.addEventListener('keyup', this.controllerRelease.bind(this));
   }
-  levels = (level) => {// returns a unique array
-    return [
-      ['player', 2],
-      ['health', 3],
-      ['stairs', 7],
-      ['weapon', 4],
-      ['enemy', 5, 100 * level],
-      ['enemy', 5, 100 * level],
-      ['health', 3],
-      ['weapon', 4],
-      ['health', 3],
-      ['enemy', 5, 100 * level],
-      ['boss', 6, 1000]
-    ];
+  componentDidUpdate() {
+   this.centerScreen();
+     if(this.state.playing === false){
+       this.timer = setInterval(this.deathCheck, 50);
+     }
+ }
+ items = (level) =>{
+   return [
+     ['player', 2,1000,1000],
+     ['health', 3],
+     ['stairs', 7],
+     ['weapon', 4],
+     ['enemy', 5,1000,1000,Math.floor(100+100*.3*level)],
+     ['enemy', 5,1000,1000,Math.floor(100+100*.3*level)],
+     ['health', 3],
+     ['weapon', 4],
+     ['health', 3],
+     ['enemy', 5,1000,1000,Math.floor(100+100*.3*level)],
+     ['boss', 6,0,0,1000]
+   ];
+ }
+ mapGen = () => {
+   var openArr = [];
+   var maxTurn = 300; // the number of turns the tunnel has
+   var maxLength = 5; // maximum number of each turn can have
+   var fulArr = this.fullArray(); // save a full array;
+   var curRow = Math.floor(Math.random() * this.state.hidth);// pick a random row
+   var curCol = Math.floor(Math.random() * this.state.hidth);// pick a random column
+   var calcObject = [[-1, 0],[1, 0],[0, -1],[0, 1]];// turns allowed are horizontal right/left and vertical right/left
+   var lastTurn = [3, 3];// a variable to save the last turn it could have any non -1,0,1 value
+   while (maxTurn) {// while number of allowed turns are not 0 do the following
+     var randTurn;// initiate variable for the turn
+     do {// do the following and check to see if passes the condition
+       randTurn = calcObject[Math.floor(Math.random() * calcObject.length)];//randomly choose a turn
+       //if the player is at the edge of the map and the random turn wants to push it off map select another turn
+       //if the turn is similar to the previously selected turn or is reverse of the previous turn select another turn
+     } while (randTurn[0] === -1 * lastTurn[0] && randTurn[1] === -1 * lastTurn[1] ||
+              randTurn[0] === lastTurn[0] && randTurn[1] === lastTurn[1]);
+     var leng = Math.ceil(Math.random() * maxLength);//randomly choose a length form the maximum allowed length
+     for (var i = 0; i < leng; i++) {//loop over the length
+         fulArr[curRow][curCol] = 0;//set the value of the item to 0
+         openArr.push([curRow, curCol]);//save this open location
+       //break the loop if it is going out of the map
+         if(curRow === 0 && randTurn[0] === -1 ||
+            curCol === 0 && randTurn[1] === -1 ||
+            curRow === this.state.hidth - 1 && randTurn[0] === 1 ||
+            curCol === this.state.hidth - 1 && randTurn[1] === 1){
+           break
+         }else{
+        //otherwise incriment the row and col according to the turn
+           curRow += randTurn[0];
+           curCol += randTurn[1];
+         }
+     }
+     lastTurn = randTurn;//set last turn to the value of the current turn
+     maxTurn--;// decrement the number of turns allowed
+   }
+
+    levels = this.items(this.state.level);
+   // find unique open spots and assign a charcter based on the levels array and the specified level
+   var uniq = this.uniqBy(openArr, JSON.stringify);
+   for (var j = 0; j < this.state.cursor; j++){
+     var randNum = Math.floor(Math.random() * uniq.length);
+     var randItem = uniq[randNum];
+     fulArr[randItem[0]][randItem[1]] = levels[j][1];
+     uniq.splice(randNum, 1);
+     if (levels[j][1] === 2){
+       levels[j][2] = randItem[0];
+       levels[j][3] = randItem[1];
+     }
+     if (levels[j][1] === 4 || levels[j][1] === 5){
+       levels[j][2] = randItem[0];
+       levels[j][3] = randItem[1];
+     }
+   }
+
+   return fulArr;// finally retun the array to be drawn
+ }
+  deathCheck = () => {
+    alert("you are dead, would you like to restart?");
+    this.restart();
+    clearInterval(this.timer);
   }
   restart = () => {
-    levelPlus6 = 6;
-    this.setState ({
-      playing: true,
-      health: 100,
-      enemHealth: 50,
-      bossHealth: 500,
-      xp: 0,
-      weapon: 1,
-      level:1,
-      message:"you are playing",
-      grid:this.mapGen()
-    });
+    location.reload();
   }
-  mapGen = () => {
-    var openArr = [];
-    var maxTurn = 300; // the number of turns the tunnel has
-    var maxLength = 5; // maximum number of each turn can have
-    var fulArr = this.fullArray(); // save a full array;
-    var curRow = Math.floor(Math.random() * hidth);// pick a random row
-    var curCol = Math.floor(Math.random() * hidth);// pick a random column
-    var calcObject = [[-1, 0],[1, 0],[0, -1],[0, 1]];// turns allowed are horizontal right/left and vertical right/left
-    var lastTurn = [3, 3];// a variable to save the last turn it could have any non -1,0,1 value
-    while (maxTurn) {// while number of allowed turns are not 0 do the following
-      var randTurn;// initiate variable for the turn
-      do {// do the following and check to see if passes the condition
-        randTurn = calcObject[Math.floor(Math.random() * calcObject.length)];//randomly choose a turn
-        //if the player is at the edge of the map and the random turn wants to push it off map select another turn
-        //if the turn is similar to the previously selected turn or is reverse of the previous turn select another turn
-      } while (randTurn[0] === -1 * lastTurn[0] && randTurn[1] === -1 * lastTurn[1] ||
-               randTurn[0] === lastTurn[0] && randTurn[1] === lastTurn[1]);
-      var leng = Math.ceil(Math.random() * maxLength);//randomly choose a length form the maximum allowed length
-      for (var i = 0; i < leng; i++) {//loop over the length
-          fulArr[curRow][curCol] = 0;//set the value of the item to 0
-          openArr.push([curRow, curCol]);//save this open location
-        //break the loop if it is going out of the map
-          if(curRow === 0 && randTurn[0] === -1 ||
-             curCol === 0 && randTurn[1] === -1 ||
-             curRow === hidth - 1 && randTurn[0] === 1 ||
-             curCol === hidth - 1 && randTurn[1] === 1){
-            break
-          }else{
-         //otherwise incriment the row and col according to the turn
-            curRow += randTurn[0];
-            curCol += randTurn[1];
-          }
-      }
-      lastTurn = randTurn;//set last turn to the value of the current turn
-      maxTurn--;// decrement the number of turns allowed
-    }
-
-     levels = items;
-    // find unique open spots and assign a charcter based on the levels array and the specified level
-    var uniq = this.uniqBy(openArr, JSON.stringify);
-    for (var j = 0; j < levelPlus6; j++){
-      var randNum = Math.floor(Math.random() * uniq.length);
-      var randItem = uniq[randNum];
-      fulArr[randItem[0]][randItem[1]] = levels[j][1];
-      uniq.splice(randNum, 1);
-      if (levels[j][1] === 2){
-        //pLocate = [randItem[0],randItem[1]];
-        levels[j][2] = randItem[0];
-        levels[j][3] = randItem[1];
-      }
-      if (levels[j][1] === 4 || levels[j][1] === 5){
-        //pLocate = [randItem[0],randItem[1]];
-        levels[j][2] = randItem[0];
-        levels[j][3] = randItem[1];
-      }
-    }
-    console.log(levels);
-    //console.log(this.state.level);
-    //console.log("levels " + levels[0][0]);
-    //console.log(this.uniqBy(openArr, JSON.stringify));
-    return fulArr;// finally retun the array to be drawn
-  }
-
   centerScreen = () => {
           var tdWidth = document.getElementById('player').clientWidth,
           tdHeight = document.getElementById('player').clientHeight,
@@ -179,7 +163,7 @@ class App extends Component {
     this.reTile(move, oldGrid);
     this.setState ({
       grid: oldGrid,
-      message: ""
+      message: "collect items and fight enemies"
     });
   }else if (this.state.grid[levels[0][2] + move[0]][levels[0][3] + move[1]] === 3) {
     oldGrid = this.state.grid;
@@ -187,7 +171,7 @@ class App extends Component {
     this.setState ({
       health: this.state.health + 100,
       grid: oldGrid,
-      message: ""
+      message: "Health item collected."
     });
   }else if (this.state.grid[levels[0][2] + move[0]][levels[0][3] + move[1]] === 4) {
     oldGrid = this.state.grid;
@@ -195,17 +179,17 @@ class App extends Component {
     this.setState ({
       weapon: this.state.weapon+ 1,
       grid: oldGrid,
-      message: ""
+      message: "Weapon effectiveness upgraded."
     });
   }else if (this.state.grid[levels[0][2] + move[0]][levels[0][3] + move[1]] === 7) {
     this.setState ({
       grid: this.mapGen(),
-      message: ""
+      message: "Teleported to another section of the dungeon."
     });
   }
   else if (this.state.grid[levels[0][2] + move[0]][levels[0][3] + move[1]] === 5) {
-    var playerDamage =  30 * (1 + this.state.weapon / 3)+Math.floor(Math.random() * 4);
-    var enemyDamage = 30 * this.state.level+Math.floor(Math.random() * 4);
+    var playerDamage = Math.floor( 30 * (1 + this.state.weapon / 3)+Math.random() * 4);
+    var enemyDamage = Math.floor(30 * this.state.level+Math.random() * 4);
     var targetEnem = 1000;
     levels.map((arr, j) => {
      if(arr[0] === "enemy" &&
@@ -220,8 +204,12 @@ class App extends Component {
       console.log("player Damag: " + playerDamage);
       console.log("player healt: " + this.state.health);
       console.log("enemy damage: " + enemyDamage);
-      alert("not enough health to take another hit. you are technically dead. :( the game will restart");
-      this.restart();
+      this.setState ({
+        playing: false,
+        health: this.state.health - enemyDamage,
+        message: "YOU ARE DEAD !!!"
+      });
+
     }else if(levels[targetEnem][4] > playerDamage){
       console.log("fighting");
       console.log("enemy health: " + levels[targetEnem][4]);
@@ -243,25 +231,23 @@ class App extends Component {
       oldGrid = this.state.grid;
       this.reTile(move, oldGrid);
       var curLevel = this.state.level;
-      if(this.state.xp % 5 === 0 && levelPlus6 < 11){
-        console.log(this.state.xp);
-        console.log(levelPlus6);
-        console.log(this.state.xp % 5);
-        console.log(levelPlus6 < 11);
+      var curCursor = this.state.cursor;
+      if(this.state.xp % 5 === 0 && this.state.cursor < 11){
         curLevel = this.state.level + 1;
-        levelPlus6++;
+        curCursor = this.state.cursor + 1;
       }
       this.setState ({
         health: this.state.health - enemyDamage,
         xp: this.state.xp + 1,
         grid: oldGrid,
         level: curLevel,
+        cursor: curCursor,
         message: "Enemy Destroyed!"
       });
     }
   }else if (this.state.grid[levels[0][2] + move[0]][levels[0][3] + move[1]] === 6) {
-    var playerDamage =  30 * (1 + this.state.weapon / 3)+Math.floor(Math.random() * 4);
-    var enemyDamage = 30 * this.state.level * 1.2 + Math.floor(Math.random() * 4);
+    var playerDamage = Math.floor( 30 * (1 + this.state.weapon / 3)+Math.random() * 4);
+    var enemyDamage = Math.floor(30 * this.state.level+Math.random() * 4);
     var targetEnem = 10;
     if(this.state.health <= enemyDamage){
       console.log("dying");
@@ -302,7 +288,7 @@ class App extends Component {
     }
 
   }
-  this.centerScreen();
+
  };
 
  damage = (levelorweapon) => {
@@ -311,9 +297,9 @@ class App extends Component {
 
   fullArray = () => {// makes an array filled with 1
     var array = [];
-    for (var i = 0; i < hidth; i++) {
+    for (var i = 0; i < this.state.hidth; i++) {
       array.push([]);
-      for (var j = 0; j < hidth; j++) {
+      for (var j = 0; j < this.state.hidth; j++) {
         array[i].push(1);
       }
     }
@@ -349,7 +335,9 @@ class App extends Component {
 
         <div id="scoreboard">
           <h1>health: {this.state.health} XP: {this.state.xp} level: {this.state.level} weapon: {this.state.weapon}</h1>
-          <h1>{this.state.message}</h1>
+          <h1 id = "message" >{this.state.message}</h1>
+          <h2 className = {"player"}>{"player"}</h2><h2 className = {"health"}>{"health"}</h2><h2 className = {"weapon"}>{"weapon"}</h2>
+          <h2 className = {"enemy"}>{"enemy"}</h2><h2 className = {"boss"}>{"boss"}</h2><h2 className = {"stairs"}>{"teleport"}</h2>
         </div>
       </div>
     );
